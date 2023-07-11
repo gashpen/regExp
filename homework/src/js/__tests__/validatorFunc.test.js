0e9c732e6b2b9bc97f25267e09ae2c5127de5111
gashpen <constantinent@mail.ru>
2023-07-11 13:45:56 +0300

    done

--- homework/src/js/__tests__/validatorFunc.test.js
+++ homework/src/js/__tests__/validatorFunc.test.js
@@ -1,1 +1,17 @@
 /* eslint-disable no-unused-vars */
+import Validator from '../validatorFunc';
+
+const newValidate = new Validator();
+
+test('test', () => {
+  expect(newValidate.validateUsername('_-Gashpen88-_').toBeFalsy());
+});
+test('test', () => {
+  expect(newValidate.validateUsername('8_-Gashpen-_8').toBeFalsy());
+});
+test('test', () => {
+  expect(newValidate.validateUsername('Gashpen8888_ZXC').toBeFalsy());
+});
+test('test', () => {
+  expect(newValidate.validateUsername('Gashpen229_ZXC-deadinside').toBeTruthy());
+});
--- homework/src/js/validatorFunc.js
+++ homework/src/js/validatorFunc.js
@@ -1,7 +1,10 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-unused-vars */
-class Validator {
-  validateUsrename(login) {
-    return /^[a-zA-Z](?!.*[\d]{4})[a-zA-Z0-9_-]*[a-zA-Z]$/g.test(login);
+export default class Validator {
+  validateUsername(login) {
+    if (/^[a-zA-Z](?!.*[\d]{4})[a-zA-Z0-9_-]*[a-zA-Z]$/g.test(login)) {
+      return true;
+    }
+    return false;
   }
 }
