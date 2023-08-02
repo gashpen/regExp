779060876d59589b37125cce55c8265c107d6947
gashpen <constantinent@mail.ru>
2023-08-02 12:38:24 +0300

    done

--- homework/src/js/__tests__/validatorFunc.test.js
+++ homework/src/js/__tests__/validatorFunc.test.js
@@ -4,14 +4,14 @@ import Validator from '../validatorFunc';
 const newValidate = new Validator();
 
 test('test', () => {
-  expect(newValidate.validateUsername('_-Gashpen88-_').toBeFalsy());
+  expect(newValidate.validateUsername('_-Gashpen88-_')).toBeFalsy();
 });
 test('test', () => {
-  expect(newValidate.validateUsername('8_-Gashpen-_8').toBeFalsy());
+  expect(newValidate.validateUsername('8_-Gashpen-_8')).toBeFalsy();
 });
 test('test', () => {
-  expect(newValidate.validateUsername('Gashpen8888_ZXC').toBeFalsy());
+  expect(newValidate.validateUsername('Gashpen8888_ZXC')).toBeFalsy();
 });
 test('test', () => {
-  expect(newValidate.validateUsername('Gashpen229_ZXC-deadinside').toBeTruthy());
+  expect(newValidate.validateUsername('Gashpen229_ZXC-deadinside')).toBeTruthy();
 });
